9efc2ba9e3b89539494ed9ceb7abd434f27e01c6
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2023-11-09 13:13:54 +0100

    Create streamlit_ap.py

---
+++ streamlit_ap.py
@@ -0,0 +1,31 @@
+import openai
+from openai import OpenAI
+import streamlit as st
+client = OpenAI()
+
+st.title("Sam-test")
+
+# Set OpenAI API key from Streamlit secrets
+openai_api_key = st.secrets["OPENAI_API_KEY"]
+
+with st.sidebar:
+    st.Text("Just an empty sidebar I might use later")
+
+st.title("💬 Talk to Sam") if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    openai.api_key = openai_api_key
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg.content)
