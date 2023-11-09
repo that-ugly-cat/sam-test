127d81233f1bda0176a82f5a6faa53a7d23767e7
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2023-11-09 11:47:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,18 +5,23 @@ client = OpenAI()
 
 st.title("Sam-test")
 
+# Set OpenAI API key from Streamlit secrets
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
+# Set a default model
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
 
+# Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+# Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
+# Accept user input
 if prompt := st.chat_input("Write to Sam"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
@@ -25,13 +30,14 @@ if prompt := st.chat_input("Write to Sam"):
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
+
+# Logic        
         for response in client.chat.completions.create(
             model=st.session_state["openai_model"],
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
-            ],
-            stream=True,
+            ]
         ):
             full_response += response['choices'][0]['message']['content']
             message_placeholder.markdown(full_response + "▌")
