1656c9b5ec33e03edb75b786eff84d53dffe56e8
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2023-11-10 10:21:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,27 +7,24 @@ client = OpenAI()
 # Set OpenAI API key from Streamlit secrets
 openai_api_key = st.secrets["OPENAI_API_KEY"]
 
-with st.sidebar:
-    st.write("Just an empty sidebar I might use later")
 
 st.title("💬 Talk to Sam") 
 if "messages" not in st.session_state:
     with open('persona.txt') as f:
         persona = f.read()
     st.session_state["messages"] = [{"role": "system", "content": persona}, {"role": "assistant", "content": "Hi, I am Sam."}]
-    ##
-    st.write(st.session_state["messages"])
-    ##
+
 
 for msg in st.session_state.messages:
-    st.chat_message(msg['role']).write(msg['content'])
+    if msg['role'] != 'system':
+        st.chat_message(msg['role']).write(msg['content'])
 
 #logic
 if prompt := st.chat_input():
     openai.api_key = openai_api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages, stream = True)
     
     msg_role = response.choices[0].message.role
     msg_content = response.choices[0].message.content
