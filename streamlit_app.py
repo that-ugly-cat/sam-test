929404ee6c8898c49b1a3acdcb999d3b27183785
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2023-11-13 16:24:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,29 +2,38 @@ import openai
 from openai import OpenAI
 import streamlit as st
 client = OpenAI()
-
-
 # Set OpenAI API key from Streamlit secrets
 openai_api_key = st.secrets["OPENAI_API_KEY"]
 
+# Define persona
+with open('persona.txt') as f:
+    persona = f.read()
 
+# Define hidden instructions
+prompt_instruction = 'Reflect on the system role before answering. Try to behave as much as possible as the person described in your system role.'
+
+# Define initial message
+initial_message = "Hi, I am Sam. I am a synthetic patient, based on the work of Giovanni Spitale, Gerold Schneider, Federico Germani, and Nikola Biller Andorno. Feel free to ask me about my experiece as a patient. Also, I speak English, Italian, German, and French."
+
+# Create interface
 st.title("💬 Talk to Sam") 
 if "messages" not in st.session_state:
-    with open('persona.txt') as f:
-        persona = f.read()
-    st.session_state["messages"] = [{"role": "system", "content": persona}, {"role": "assistant", "content": "Hi, I am Sam. I am a synthetic patient, based on the work of Giovanni Spitale, Gerold Schneider, Federico Germani, and Nikola Biller Andorno. Feel free to ask me about my experiece as a patient. Also, I speak English, Italian, German, and French."}]
+    st.session_state["messages"] = [{"role": "system", "content": persona}, {"role": "assistant", "content": initial_message}]
 
+# Show messages
 
 for msg in st.session_state.messages:
     if msg['role'] != 'system':
         st.chat_message(msg['role']).write(msg['content'])
+        if msg['role'] == 'assistant':
+            msg = msg.replace(initial_message, '')
 
-#logic
+
+# Logic
 if prompt := st.chat_input():
     openai.api_key = openai_api_key
-    prompt_instruction = 'Reflect on the system role before answering. Try to behave as much as possible as the person described in your system role.'
     prompt_send = prompt_instruction + ' ' + prompt
-    st.session_state.messages.append({"role": "user", "content": prompt_send})
+    st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
     
